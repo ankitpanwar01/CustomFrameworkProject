70492cd004ecf4b1eeb58b137850c5dad3cb7ba2
Ankit Panwar <ankit.panwarmca@gmail.com>
2023-08-08 17:21:04 +0530

    swift package added

---
+++ CustomFrameworkProject/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Ankit Panwar on 08/08/23.
+//
+
+import Foundation
+
+
+//public struct SDK {
+//    public static func doSomeWork() {
+//        print("Doing some work... ")
+//    }
+//}
